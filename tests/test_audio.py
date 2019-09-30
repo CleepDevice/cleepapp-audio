0073906b1965ff7912785e3ab7a2539e3cb32798
Tangb <tanguy.bonneau@gmail.com>
2019-09-30 17:58:02 +0200

    Add tests dir. Update .gitignore

---
+++ tests/test_audio.py
@@ -0,0 +1,24 @@
+import unittest
+import logging
+import sys
+sys.path.append('../')
+from backend.audio import Audio
+from raspiot.utils import InvalidParameter, MissingParameter, CommandError, Unauthorized
+from raspiot.libs.tests import session
+import os
+import time
+from mock import Mock
+
+class TestAudio(unittest.TestCase):
+
+    def setUp(self):
+        self.session = session.Session(logging.CRITICAL)
+        _mod = Audio
+        self.module = self.session.setup(_mod)
+
+    def tearDown(self):
+        self.session.clean()
+
+if __name__ == "__main__":
+    unittest.main()
+    

7a6d930b0386be9e6561659f2b1d4e0b3e25d60f
Tangb <tanguy.bonneau@gmail.com>
2020-12-07 23:12:21 +0100

    Clean code

--- backend/audio.py
+++ backend/audio.py
@@ -18,38 +18,38 @@ class Audio(CleepResources):
     """
     Audio module is in charge of configuring audio on raspberry pi
     """
-    MODULE_AUTHOR = u'Cleep'
-    MODULE_VERSION = u'1.1.0'
-    MODULE_CATEGORY = u'APPLICATION'
+    MODULE_AUTHOR = 'Cleep'
+    MODULE_VERSION = '1.1.0'
+    MODULE_CATEGORY = 'APPLICATION'
     MODULE_PRICE = 0
     MODULE_DEPS = []
-    MODULE_DESCRIPTION = u'Configure audio on your device'
-    MODULE_LONGDESCRIPTION = u'Application that helps you to configure audio on your device'
-    MODULE_TAGS = [u'audio', u'sound']
+    MODULE_DESCRIPTION = 'Configure audio on your device'
+    MODULE_LONGDESCRIPTION = 'Application that helps you to configure audio on your device'
+    MODULE_TAGS = ['audio', 'sound']
     MODULE_COUNTRY = None
-    MODULE_URLINFO = u'https://github.com/tangb/cleepmod-audio'
-    MODULE_URLHELP = u'https://github.com/tangb/cleepmod-audio/wiki'
-    MODULE_URLBUGS = u'https://github.com/tangb/cleepmod-audio/issues'
+    MODULE_URLINFO = 'https://github.com/tangb/cleepmod-audio'
+    MODULE_URLHELP = 'https://github.com/tangb/cleepmod-audio/wiki'
+    MODULE_URLBUGS = 'https://github.com/tangb/cleepmod-audio/issues'
     MODULE_URLSITE = None
 
-    MODULE_CONFIG_FILE = u'audio.conf'
+    MODULE_CONFIG_FILE = 'audio.conf'
     DEFAULT_CONFIG = {
-        u'driver': None
+        'driver': None
     }
 
-    TEST_SOUND = u'/opt/cleep/sounds/connected.wav'
+    TEST_SOUND = '/opt/cleep/sounds/connected.wav'
 
     DEFAULT_DEVICE = {
-        u'card': 0,
-        u'device': 0
+        'card': 0,
+        'device': 0
     }
 
     MODULE_RESOURCES = {
-        u'audio.playback': {
-            u'permanent': False,
+        'audio.playback': {
+            'permanent': False,
         },
-        u'audio.capture': {
-            u'permanent': False,
+        'audio.capture': {
+            'permanent': False,
         }
     }
 
@@ -79,30 +79,30 @@ class Audio(CleepResources):
         Module configuration
         """
         # restore selected soundcard
-        selected_driver_name = self._get_config_field(u'driver')
+        selected_driver_name = self._get_config_field('driver')
         self.logger.trace('selected_driver_name=%s audio supported=%s' % (
             selected_driver_name,
-            Tools.raspberry_pi_infos()[u'audio']
+            Tools.raspberry_pi_infos()['audio']
         ))
-        if not selected_driver_name and Tools.raspberry_pi_infos()[u'audio']:
+        if not selected_driver_name and Tools.raspberry_pi_infos()['audio']:
             # set default sound driver to raspberry pi embedded one
             self.logger.trace('Set default sound driver')
             selected_driver_name = self.bcm2835_driver.name
-            self._set_config_field(u'driver', self.bcm2835_driver.name)
+            self._set_config_field('driver', self.bcm2835_driver.name)
 
         if selected_driver_name is None:
             # still no selected driver name, it means audio is not supported on this board
-            self.logger.info(u'No audio supported on this device')
+            self.logger.info('No audio supported on this device')
             return
-        self.logger.debug(u'Selected audio driver name "%s"' % selected_driver_name)
+        self.logger.debug('Selected audio driver name "%s"' % selected_driver_name)
 
         # get selected driver
         driver = self.drivers.get_driver(Driver.DRIVER_AUDIO, selected_driver_name)
 
         # fallback to default driver if necessary (and possible)
-        if not driver and Tools.raspberry_pi_infos()[u'audio']:
+        if not driver and Tools.raspberry_pi_infos()['audio']:
             self.logger.warning('Configured audio driver is not loaded, fallback to default one.')
-            self._set_config_field(u'driver', self.bcm2835_driver.name)
+            self._set_config_field('driver', self.bcm2835_driver.name)
             driver = self.drivers.get_driver(Driver.DRIVER_AUDIO, self.bcm2835_driver.name)
 
         # enable driver if possible
@@ -110,11 +110,11 @@ class Audio(CleepResources):
             self.logger.info('No driver found while it should')
             return
         if not driver.is_installed():
-            self.logger.error(u'Unable to enable soundcard because it is not properly installed. Please install it manually.')
+            self.logger.error('Unable to enable soundcard because it is not properly installed. Please install it manually.')
         elif not driver.is_enabled():
-            self.logger.info(u'Enabling audio driver "%s"' % driver.name)
+            self.logger.info('Enabling audio driver "%s"' % driver.name)
             if not driver.enable():
-                self.logger.error(u'Unable to enable soundcard. Internal driver error.')
+                self.logger.error('Unable to enable soundcard. Internal driver error.')
 
     def get_module_config(self):
         """
@@ -132,33 +132,33 @@ class Audio(CleepResources):
         playbacks = []
         captures = []
         volumes = {
-            u'playback': None,
-            u'capture': None,
+            'playback': None,
+            'capture': None,
         }
 
         audio_drivers = self.drivers.get_drivers(Driver.DRIVER_AUDIO)
         for driver_name, driver in audio_drivers.items():
             device_infos = driver.get_device_infos()
             device = {
-                u'name': driver.card_name,
-                u'label': driver_name,
-                u'device': device_infos,
-                u'enabled': driver.is_enabled(),
-                u'installed': driver.is_installed(),
+                'name': driver.card_name,
+                'label': driver_name,
+                'device': device_infos,
+                'enabled': driver.is_enabled(),
+                'installed': driver.is_installed(),
             }
-            if device_infos[u'playback']:
+            if device_infos['playback']:
                 playbacks.append(device)
-            if device_infos[u'capture']:
+            if device_infos['capture']:
                 captures.append(device)
-            if device[u'enabled'] and device[u'installed']:
+            if device['enabled'] and device['installed']:
                 volumes = driver.get_volumes()
 
         return {
-            u'devices': {
-                u'playback': sorted(playbacks, key=lambda k: k[u'label']),
-                u'capture': sorted(captures, key=lambda k: k[u'label']),
+            'devices': {
+                'playback': sorted(playbacks, key=lambda k: k['label']),
+                'capture': sorted(captures, key=lambda k: k['label']),
             },
-            u'volumes': volumes
+            'volumes': volumes
         }
 
     def select_device(self, driver_name):
@@ -176,37 +176,37 @@ class Audio(CleepResources):
         """
         # check params
         if driver_name is None or len(driver_name) == 0:
-            raise MissingParameter(u'Parameter "driver_name" is missing')
+            raise MissingParameter('Parameter "driver_name" is missing')
 
         # get drivers
-        selected_driver_name = self._get_config_field(u'driver')
+        selected_driver_name = self._get_config_field('driver')
         old_driver = self.drivers.get_driver(Driver.DRIVER_AUDIO, selected_driver_name) if selected_driver_name is not None else None
         new_driver = self.drivers.get_driver(Driver.DRIVER_AUDIO, driver_name)
 
         if not new_driver:
-            raise InvalidParameter(u'Specified driver does not exist')
+            raise InvalidParameter('Specified driver does not exist')
         if not new_driver.is_installed():
-            raise InvalidParameter(u'Can\'t selected device because its driver seems not to be installed')
+            raise InvalidParameter('Can\'t selected device because its driver seems not to be installed')
 
         # disable old driver
-        self.logger.info(u'Using audio driver "%s"' % new_driver.name)
+        self.logger.info('Using audio driver "%s"' % new_driver.name)
         if old_driver and old_driver.is_installed():
             disabled = old_driver.disable()
-            self.logger.debug(u'Disable previous driver "%s": %s' % (old_driver.name, disabled))
+            self.logger.debug('Disable previous driver "%s": %s' % (old_driver.name, disabled))
             if not disabled:
-                raise CommandError(u'Unable to disable current device')
+                raise CommandError('Unable to disable current device')
 
         # enable new driver
-        self.logger.debug(u'Enable new driver "%s"' % new_driver.name)
+        self.logger.debug('Enable new driver "%s"' % new_driver.name)
         driver_enabled = new_driver.enable()
         if not driver_enabled or not new_driver.is_card_enabled():
-            self.logger.debug(u'Unable to enable new driver. Revert re-enabling old driver')
+            self.logger.debug('Unable to enable new driver. Revert re-enabling old driver')
             if old_driver:
                 old_driver.enable()
-            raise CommandError(u'Unable to enable selected device')
+            raise CommandError('Unable to enable selected device')
 
         # everything is fine, save new driver
-        self._set_config_field(u'driver', new_driver.name)
+        self._set_config_field('driver', new_driver.name)
 
     def set_volumes(self, playback, capture):
         """
@@ -236,12 +236,12 @@ class Audio(CleepResources):
         if capture < 0 or capture > 100:
             raise InvalidParameter('Parameter "capture" must be a valid percentage')
 
-        self.logger.info(u'Set volumes to: playback[%s%%] capture[%s%%]' % (playback, capture))
+        self.logger.info('Set volumes to: playback[%s%%] capture[%s%%]' % (playback, capture))
 
-        selected_driver_name = self._get_config_field(u'driver')
+        selected_driver_name = self._get_config_field('driver')
         volumes = {
-            u'playback': None,
-            u'capture': None,
+            'playback': None,
+            'capture': None,
         }
 
         # no driver configured
@@ -265,14 +265,14 @@ class Audio(CleepResources):
         Play test sound to make sure audio card is correctly configured
         """
         # request playback resource
-        self._need_resource(u'audio.playback')
+        self._need_resource('audio.playback')
 
     def test_recording(self):
         """
         Record sound during few seconds and play it
         """
         # request capture resource (non blocking)
-        self._need_resource(u'audio.capture')
+        self._need_resource('audio.capture')
 
         # pause to simulate 5 seconds recording
         time.sleep(5.0)
@@ -288,33 +288,33 @@ class Audio(CleepResources):
             CommandError: if command failed
         """
         self.logger.debug('Resource "%s" acquired' % resource_name)
-        if resource_name == u'audio.playback':
+        if resource_name == 'audio.playback':
             # play test sample
             if not self.alsa.play_sound(self.TEST_SOUND):
-                raise CommandError(u'Unable to play test sound: internal error')
+                raise CommandError('Unable to play test sound: internal error')
 
             # release resource
-            self._release_resource(u'audio.playback')
+            self._release_resource('audio.playback')
 
-        elif resource_name == u'audio.capture':
+        elif resource_name == 'audio.capture':
             # record sound
             sound = self.alsa.record_sound(timeout=5.0)
-            self.logger.debug(u'Recorded sound: %s' % sound)
+            self.logger.debug('Recorded sound: %s' % sound)
             if not self.alsa.play_sound(sound, timeout=6.0):
-                raise CommandError(u'Unable to play recorded sound: internal error')
+                raise CommandError('Unable to play recorded sound: internal error')
 
             # release resource
-            self._release_resource(u'audio.capture')
+            self._release_resource('audio.capture')
 
             # purge file
             time.sleep(0.5)
             try:
                 os.remove(sound)
             except Exception:
-                self.logger.warning(u'Unable to delete recorded test sound "%s"' % sound)
+                self.logger.warning('Unable to delete recorded test sound "%s"' % sound)
 
         else:
-            self.logger.error(u'Unsupported resource "%s" acquired' % resource_name)
+            self.logger.error('Unsupported resource "%s" acquired' % resource_name)
 
     def _resource_needs_to_be_released(self, resource_name): # pragma: no cover
         """
